8719c9970cd91ab75d7b24fdf1a18ab99dd369f5
Rick Kuipers <rick@wedevelop.nl>
2023-12-06 17:52:29 +0100

    Demo 2 Add encore and pretty files

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ up:
 	symfony server:start -d
 	symfony console messenger:setup-transports
 	symfony run -d --watch=config,src,templates,vendor symfony console messenger:consume async
+	symfony run -d npx encore dev-server
 
 down:
 	docker compose down
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -10,6 +10,7 @@ return array(
     'Webmozart\\Assert\\' => array($vendorDir . '/webmozart/assert/src'),
     'Twig\\Extra\\TwigExtraBundle\\' => array($vendorDir . '/twig/extra-bundle'),
     'Twig\\' => array($vendorDir . '/twig/twig/src'),
+    'Symfony\\WebpackEncoreBundle\\' => array($vendorDir . '/symfony/webpack-encore-bundle/src'),
     'Symfony\\Runtime\\Symfony\\Component\\' => array($vendorDir . '/symfony/runtime/Internal'),
     'Symfony\\Polyfill\\Php83\\' => array($vendorDir . '/symfony/polyfill-php83'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
